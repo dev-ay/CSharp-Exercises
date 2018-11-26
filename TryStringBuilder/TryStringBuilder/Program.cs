0f7c5b60f81f3b623e530ea3ddd1ec095c0fc7b5
dev-ay <44041117+dev-ay@users.noreply.github.com>
2018-11-25 18:11:37 -0800

    Add TryStringBuilder

---
+++ TryStringBuilder/TryStringBuilder/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TryStringBuilder
+{
+    class Program
+    {
+        static void Main()
+        {
+            String A = "Hi! ";
+            String B = "StringBuilder is a library tool created by Microsoft, ";
+            String C = "using the C# language.";
+
+            StringBuilder sb = new StringBuilder();
+
+            sb.Append(A);
+            sb.Append(B);
+            sb.Append(C);
+
+            Console.WriteLine(sb.ToString().ToUpper());
+
+            Console.ReadLine();
+        }
+    }
+}
