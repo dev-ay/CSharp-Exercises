9850c4d287e1ffc7e89cf6f9324e1400fe9f815a
dev-ay <44041117+dev-ay@users.noreply.github.com>
2018-11-28 21:44:43 -0800

    Add OutParameter
    Exercise also implements static method

---
+++ OutParameter/OutParameter/Math.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OutParameter
+{
+    class Math
+    {
+
+        public int divide(int x)
+        {
+            return x / 2;
+        }
+
+
+        public void divide(int x, out int result)
+        {
+            result = x / 2;
+        }
+
+
+        public static int Divide(int x)
+        {
+            return x / 2;
+        }
+
+
+    }
+}
---
+++ OutParameter/OutParameter/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OutParameter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter an integer to divide by 2: ");
+            int dividend = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine();
+            Console.WriteLine();
+
+            Math m = new Math();
+            int A = m.divide(dividend);
+            Console.WriteLine("Return method: " + A);
+            Console.WriteLine();
+
+            int B;
+            m.divide(dividend, out B);
+            Console.WriteLine("Void method (overload): " + B);
+            Console.WriteLine();
+
+            int C = Math.Divide(dividend);
+            Console.WriteLine("Static method: " + C);
+            Console.WriteLine();
+
+
+            Console.ReadLine();
+        }
+    }
+}
